166ef10d9bc3029db47677b55f532a3a6c016d11
PierreTL <pierre.tampon@gmail.com>
2025-04-30 09:41:24 +0200

    Récupération data + rendu

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
-module.exports = function (eleventyConfig) {
+const fs = require('fs');
+const matter = require('gray-matter');
+
+module.exports = async  function (eleventyConfig) {
+
+    //obtenir le contenu uniquement d'un fichier md sans le front matter
+    eleventyConfig.addFilter("contentOnly", function(filePath) {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        const { content } = matter(fileContent);
+        return content;
+    });
+    
+    //obtenir le contenu d'un fichier md
+    const { RenderPlugin } = await import("@11ty/eleventy");
+    eleventyConfig.addPlugin(RenderPlugin);
+
+
+    //config global
     return {
         passthroughFileCopy: true,
         dir: {
